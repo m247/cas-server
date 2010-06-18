80c3a9966fb3d93d805c31cca4c7198a5c61338a
Geoff Garside <geoff@geoffgarside.co.uk>
2010-06-18 10:49:55 +0100

    Add Validate::Service functionality

--- lib/validate.rb
+++ lib/validate.rb
@@ -5,6 +5,9 @@ module Validate
   def self.plain(&blk)
     Plain.new(&blk).call(@app)
   end
+  def self.service(&blk)
+    Service.new(&blk).call(@app)
+  end
 
   class Plain
     def initialize(&blk)
@@ -30,6 +33,67 @@ module Validate
     private
       def renew?
         @params['renew'] == 'true'
+      end
+  end
+  class Service
+    def initialize(&blk)
+      instance_eval(&blk) if blk
+    end
+    def success(&blk)
+      @success = blk
+    end
+    def failure(&blk)
+      @failure = blk
+    end
+    def call(app)
+      @params = app.params
+      begin
+        st = ServiceTicket.validate!(app.params['ticket'], app.params['service'], renew?)
+
+        verify_proxy_callback(st) do |pgt, res|
+          if %w(200 202 301 302 304).include?(res.code)
+            pgt.save
+          end
+        end
+
+        @success.call(st.username, st.proxy_granting_ticket)
+      rescue Exception => e
+        @failure.call(e.message)
+      end
+    end
+    protected
+      def params
+        @params
+      end
+      def renew?
+        params['renew'] == 'true'
+      end
+      # TODO: Check the code down here
+      def proxy_granting_ticket(service_ticket)
+        ProxyGrantingTicket.new(:service_ticket => service_ticket,
+          :iou => ProxyGrantingTicketIOU.new)
+      end
+      def proxy_callback?
+        params['pgtUrl'] && params['pgtUrl'] =~ %r{^https://}
+      end
+      def verify_proxy_callback(service_ticket)
+        return unless proxy_callback?
+        begin
+          pgt = proxy_granting_ticket(service_ticket)
+          uri = URI.parse(params['pgtUrl'])
+          https = Net::HTTP.new(uri.host, uri.port)
+          https.use_ssl = true
+          https.ca_file = options.ca_file
+          https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+          https.start do
+            uri.query = uri.query ? uri.query + "&#{pgt.to_query_string}" : pgt.to_query_string
+            https.request_get(uri.request_uri) do |res|
+              yield pgt, res
+            end
+          end
+        rescue OpenSSL::SSL::SSLError
+          return
+        end
       end
   end
 end
---
+++ spec/validate_service_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+module Validate
+  describe Service do
+    before(:each) do
+      @service = Service.new do
+        success { 'success' }
+        failure { 'failure' }
+      end
+    end
+    describe ".new" do
+      it "should set success proc" do
+        @service.instance_variable_get("@success").should_not be_nil
+      end
+      it "should set failure proc" do
+        @service.instance_variable_get("@failure").should_not be_nil
+      end
+    end
+    describe "#call" do
+      before(:each) do
+        @app = double('Sinatra::Base app')
+        @app.stub(:params).and_return({})
+      end
+      describe "invalid service ticket" do
+        it "should return failure" do
+          @service.call(@app).should == 'failure'
+        end
+      end
+      describe "valid service ticket" do
+        before(:each) do
+          @ticket = double('ServiceTicket')
+          @ticket.stub(:username).and_return('test')
+          @ticket.stub(:proxy_granting_ticket).and_return(nil)
+          ServiceTicket.stub!(:validate!).and_return(@ticket)
+        end
+        it "should return success" do
+          @service.call(@app).should == 'success'
+        end
+      end
+    end
+  end
+end
