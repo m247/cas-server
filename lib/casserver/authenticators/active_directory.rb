284f0052731b2a55373723d79f6bcf50b6ac2d33
Geoff Garside <geoff@geoffgarside.co.uk>
2010-07-06 09:45:47 +0100

    Add :locked callback proc to ActiveDirectory authenticator to lock accounts with ACCOUNTDISABLE flag set

--- lib/casserver/authenticators/active_directory.rb
+++ lib/casserver/authenticators/active_directory.rb
@@ -2,7 +2,9 @@ module CASServer
   module Authenticator
     class ActiveDirectory < LDAP
       def self.defaults
-        superclass.defaults.merge({:user_attr => 'sAMAccountName'})
+        # Locked checks to see if ACCOUNTDISABLE flag is set on userAccountControl attribute
+        superclass.defaults.merge({:user_attr => 'sAMAccountName',
+          :locked => proc { |r| r['userAccountControl'] & 0x002 == 0x002 }})
       end
     end
   end
