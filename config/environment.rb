9504496793dbb452cb82b04f5cac5683fc12b36d
Geoff Garside <geoff@geoffgarside.co.uk>
2010-07-14 11:41:04 +0100

    Add public path to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 CASServer::Application.configure do |app|
   app.set :root, File.expand_path('../../', __FILE__)
   app.set :views, File.join(app.root, 'views')
+  app.set :public, File.join(app.root, 'public')
   app.set :translations, File.join(app.root, 'config', 'i18n')
 
   LOGGER = Logger.new(File.join(app.root, "sinatra.log"))
