1b64771ec726b7e4e8b1deeada734be797294c8a
Geoff Garside <geoff@geoffgarside.co.uk>
2010-07-15 11:26:36 +0100

    Support showing the #username-field:hover label on #username:focus as well

--- public/js/default.js
+++ public/js/default.js
@@ -5,4 +5,16 @@ jQuery(document).ready(function($) {
       btn.val(btn.attr('data-disable_with'));
     btn.attr('disabled', 'disabled');
   });
+
+  if ( $('#username') ) {
+    $('#username').focusin(function(ev) {
+      var name = $(ev.target).attr('name');
+      $("label[for="+ name +"]").addClass('focused');
+    });
+    $('#username').focusout(function(ev) {
+      var name = $(ev.target).attr('name');
+      $("label[for="+ name +"]").removeClass('focused');
+    });
+    $('#username').focus();
+  }
 });
