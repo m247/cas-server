f1b5573a39524eb8616c0e84967dbbc6f228f107
Geoff Garside <geoff@geoffgarside.co.uk>
2010-07-15 09:31:33 +0100

    Correct the ActiveDirectory locked proc to process the userAccountControl value properly

--- lib/casserver/authenticators/active_directory.rb
+++ lib/casserver/authenticators/active_directory.rb
@@ -4,7 +4,7 @@ module CASServer
       def self.defaults
         # Locked checks to see if ACCOUNTDISABLE flag is set on userAccountControl attribute
         superclass.defaults.merge({:user_attr => 'sAMAccountName',
-          :locked => proc { |r| r['userAccountControl'] & 0x002 == 0x002 }})
+          :locked => proc { |r| r['userAccountControl'][0].to_i & 0x002 == 0x002 }})
       end
     end
   end
